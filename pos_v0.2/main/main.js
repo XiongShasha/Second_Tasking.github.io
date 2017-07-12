913d540cfd1330f1d25aa1acc545a50064457e1b
Your Name <2246824391@qq.com>
2017-07-12 17:09:58 +0800

    pos0.2

--- pos_v0.2/main/main.js
+++ pos_v0.2/main/main.js
@@ -1,48 +1,63 @@
 'use strict';
-function ChangeAr(inputs)
-{
-  //#1. 讲一维数组转换为对象数组
-  var ChangeArr=loadAllItems();
-  for(let i=0;i<inputs.lenght;i++)
-   {
-    for(let j=0;j>ChangeArr.length;j++)
-     {
-       if(ChangeArr[j]==inputs[i])
-        {
-          inputs[i]=ChangeArr[j];
-        }
-     }
-   }
-}
-function printReceipt(inputs) {
 
-//#2.计算小计
-  var per_Sum = new Array();
-  for(var i=0;i<inputs.length;i++)
+//#4.打印
+function printReceipt(inputs)
+{
+//#1.调用loadAllItems()函数得到一个新的商品清单inputsArr：[Object]，再通过与传入的barcode联系，得到用户购买物品的详细清单，并计算商品数量
+var inputsArr=loadAllItems();
+ var Com_inputs=[{}];
+ var res=[];
+ for(let i=0;i<inputs.length;)//计算inputs数组内重复的元素以及重复的个数
   {
-    if(inputs[i].barcode=='ITEM000000'||inputs[i].barcode=='ITEM000001'||inputs[i].barcode=='ITEM000004')
-    {
-      per_Sum[i]=(inputs[i].count)*(inputs[i].price);
+   var count =0;
+   for(let j=i;j<inputs.length;j++)
+    { 
+      if(inputs[j]=inputs[i])
+      {count++;}
     }
-  }
-//#3.计算总金额
-  var tol_Sum=0;
-  for(var i=0;i<per_Sum.length;i++)
+    res.push([inputs[i],count]);
+    i+=count;
+  } 
+  for(let j=i+1;j<inputs.length;j++)//将重复的元素删除
+      if(inputs[j]=inputs[i])
+        for(let k=j+1;k<inputs.length;k++)
+          inputs[k-1]=inputs[k];
+  for(let i=0;i<inputs.length;i++) //构建出一个新的包含所有商品信息的数组
   {
-    if(inputs[i].barcode=='ITEM000000'||inputs[i].barcode=='ITEM000001'||inputs[i].barcode=='ITEM000004')
-    {
+    for(let j=0;j<inputsArr.length;j++)
+      if(inputs[i]==inputsArr[j].barcode)
+      {
+         Com_inputs[i]=inputsArr[j];
+         Com_inputs[i].count=res[i][1];
+      } 
+  }
+//#2.计算小计
+function S_Sum(Com_inputs)
+{
+  var per_Sum=[];
+  for(let i=0;i<Com_inputs.length;i++)
+  {
+    per_Sum[i]=Com_inputs[i].count*Com_inputs[i].price;
+  }
+  return per_Sum;
+}
+//#3.计算总金额
+  var per_Sum=S_Sum(Com_inputs);
+  function t_Sum(per_Sum)
+  {
+    var tol_Sum=0;
+    for(var i=0;i<per_Sum.length;i++)
+    { 
       tol_Sum+=per_Sum[i];
     }
+    return tol_Sum;
   }
-//#4.打印
-var str='';
+var tol_Sum=t_Sum(per_Sum);
+  var str='';
   for(var i=0;i<inputs.length;i++)
   {
-    if(inputs[i].barcode=='ITEM000000'||inputs[i].barcode=='ITEM000001'||inputs[i].barcode=='ITEM000004')
-    {
-     str+='名称：'+inputs[i].name+'，数量：'+inputs[i].count+
-         inputs[i].unit+'，单价：'+inputs[i].price+'(元)'+'，小计：'+per_Sum[i].toFixed(2)+'(元)\n';
-    }
+     str+='名称：'+Com_inputs[i].name+'，数量：'+Com_inputs[i].count+
+        Com_inputs[i].unit+'，单价：'+Com_inputs[i].price+'(元)'+'，小计：'+per_Sum[i].toFixed(2)+'(元)\n';
   }
     console.log('***<没钱赚商店>收据***\n'+str
     +'----------------------\n'+'总计：'+tol_Sum.toFixed(2)+'(元)\n'+'**********************');
